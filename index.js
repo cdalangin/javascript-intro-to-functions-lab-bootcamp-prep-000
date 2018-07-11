51f9bdaf066e114d248f52439ce693065e21de01
Cirill Florenz A Dalangin <florenzdalangin@gmail.com>
2018-07-11 13:16:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if(string.toUpperCase()) {
+  return "YES INDEED!"
+  }
+  if(string.toLowerCase()) {
+  return "I can't hear you!"
+  }
+  if(string==="I love u, grandma.") {
+  return "I love you, too."
+  }
+}
